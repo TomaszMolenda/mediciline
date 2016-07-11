d18ac4de29ccb3243d44a830c1ea0bf754038a25
tomo <tomasz.molenda@gmail.com>
2016-07-11 11:29:56 +0200

    refactoring rest medicament controller

--- src/main/java/pl/tomo/controller/rest/RestMedicamentController.java
+++ src/main/java/pl/tomo/controller/rest/RestMedicamentController.java
@@ -1,14 +1,11 @@
 package pl.tomo.controller.rest;
 
-import java.security.Principal;
 import java.util.List;
 
 import javax.persistence.NoResultException;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.monitorjbl.json.JsonResult;
 import com.monitorjbl.json.JsonView;
@@ -37,15 +33,12 @@ import pl.tomo.medicament.entity.ProductType;
 import pl.tomo.medicament.service.MedicamentAdditionalService;
 import pl.tomo.medicament.service.MedicamentMService;
 import pl.tomo.service.MedicamentService;
-import pl.tomo.service.RequestService;
 import pl.tomo.service.UserService;
 
 @Controller
 @RequestMapping(value = "/api")
 public class RestMedicamentController {
-	
-	private Logger logger = Logger.getLogger(RestMedicamentController.class);
-	
+		
 	private JsonResult json = JsonResult.instance();
 	
 	@Autowired
@@ -90,79 +83,6 @@ public class RestMedicamentController {
 		else throw new UserNotFoundException(request);
 	}
 	
-	@RequestMapping(value = "/medicaments", headers="Accept=application/json")
-	@ResponseBody
-	public void getMedicaments(HttpServletRequest request) {
-		List<Medicament> medicaments = medicamentService.findAll(request);
-			json.use(JsonView.with(medicaments).onClass(Medicament.class, Match.match().exclude("*")
-					.include("idServer")
-					.include("name")
-					.include("producent")
-					.include("price")
-					.include("kind")
-					.include("date")
-					.include("productLineID")
-					.include("packageID")));
-			
-	}
-	
-	
-	@RequestMapping(value = "/medicamentsdb", headers="Accept=application/json")
-	@ResponseBody
-	public void getMedicamentsDb() {
-		
-		List<pl.tomo.medicament.entity.Medicament> medicaments = medicamentMService.getAllMedicaments();
-
-		json.use(JsonView.with(medicaments).onClass(pl.tomo.medicament.entity.Medicament.class, Match.match()
-				.exclude("atcs")
-				.exclude("distributor")
-				.exclude("productType")
-				.exclude("prescription")
-				.exclude("diseases"))
-					.onClass(MedicamentAdditional.class, Match.match().exclude("medicaments")));
-	}
-	
-	@RequestMapping(value = "/medicamentsdb/additional", headers="Accept=application/json")
-	@ResponseBody
-	public void getMedicamentsDbAdditional() {
-		
-		List<MedicamentAdditional> list = medicamentAdditionalService.findAll();
-
-		json.use(JsonView.with(list).onClass(MedicamentAdditional.class, Match.match()
-				.exclude("medicaments")));
-	}
-	
-	
-	@RequestMapping(value = "/medicamentsdb/count", headers="Accept=application/json")
-	@ResponseBody
-	public Integer getMedicamentsDbCount() {
-		
-		List<pl.tomo.medicament.entity.Medicament> medicaments = medicamentMService.getAllMedicaments();
-		return medicaments.size();
-		
-	}
-	
-	@RequestMapping(value="/medicamentsdb/information", method = RequestMethod.GET)
-	@ResponseBody
-	public void getMedicamentAdditionalInJSON(ModelMap modelMap, @RequestParam("packageID") int packageID, HttpServletRequest request, Principal principal) {
-			pl.tomo.medicament.entity.Medicament medicamentM = null;
-			try {
-				medicamentM = medicamentMService.getMedicamentByPackageID(packageID);
-			} catch (NoResultException e) {
-				logger.info("User " + principal.getName() + "  try get medicament information json (database) - no success - medicament edited");
-				return;
-			}
-			if(medicamentM != null) {
-				json.use(JsonView.with(medicamentM).onClass(pl.tomo.medicament.entity.Medicament.class, Match.match())
-						.onClass(MedicamentAdditional.class, Match.match().exclude("medicaments"))
-						.onClass(ATC.class, Match.match().exclude("medicaments"))
-						.onClass(Distributor.class, Match.match().exclude("medicaments"))
-						.onClass(ProductType.class, Match.match().exclude("medicaments"))
-						.onClass(Prescription.class, Match.match().exclude("medicaments"))
-						.onClass(Disease.class, Match.match().exclude("medicaments")));
-				logger.info("User " + principal.getName() + " get medicament information json (database), medicament packageID: " + medicamentM.getPackageID());
-			}
-	}
 	
 	@RequestMapping(value="/medicamentsdb/search", method = RequestMethod.GET, headers="Accept=application/json")
 	public @ResponseBody void getMedicamentsDbInJSON(@RequestParam("search") String search, HttpServletRequest request) {
@@ -180,8 +100,7 @@ public class RestMedicamentController {
 					.include("producer")
 					.include("dosageObject"))
 					.onClass(Dosage.class, Match.match().include("*")));
-			logger.info("User " + user.getName() + " get medicaments json (database) - search: " + search);
-		}
+			}
 		
 	}
 	
@@ -192,7 +111,6 @@ public class RestMedicamentController {
 			try {
 				medicamentM = medicamentMService.getMedicament(id, request);
 			} catch (NoResultException e) {
-				logger.info("Ip " + request.getRemoteAddr() + "  try get medicament information json (database) - no success");
 				throw new AdditionalInformationException(request);
 			}
 			if(medicamentM != null) {
@@ -203,7 +121,6 @@ public class RestMedicamentController {
 						.onClass(ProductType.class, Match.match().exclude("medicaments"))
 						.onClass(Prescription.class, Match.match().exclude("medicaments"))
 						.onClass(Disease.class, Match.match().exclude("medicaments")));
-				logger.info("Ip " + request.getRemoteAddr() + " get medicament information json (database), medicament packageID: " + medicamentM.getPackageID());
-			}
+				}
 	}
 }
